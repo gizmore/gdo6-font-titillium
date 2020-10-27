97acbdd03a7c2e42f3f5e49d4f7b1b1754e2bbe9
gizmore <gizmore@wechall.net>
2020-10-27 22:19:07 +0100

    Init

---
+++ Module_FontTitillium.php
@@ -0,0 +1,53 @@
+<?php
+namespace GDO\FontTitillium;
+
+use GDO\Core\GDO_Module;
+use GDO\UI\GDT_FontWeight;
+use GDO\DB\GDT_Checkbox;
+
+/**
+ * Includes Titillium Font CSS on init.
+ * Do not forget to run yarn.sh before installation.
+ * @author gizmore
+ * @version 6.10
+ * @since 6.10
+ * @license OFL
+ */
+final class Module_FontTitillium extends GDO_Module
+{
+    ##############
+    ### Module ###
+    ##############
+    public $module_priority = 90;
+    
+    public $module_license = "OFL";
+    
+    public function onLoadLanguage() { return $this->loadLanguage('lang/titillium'); }
+    
+    ##############
+    ### Config ###
+    ##############
+    public function getConfig()
+    {
+        return array(
+            GDT_FontWeight::make('font_weight')->initial('400'),
+            GDT_Checkbox::make('font_include')->initial('1'),
+        );
+    }
+    public function cfgFontWeight() { return $this->getConfigVar('font_weight'); }
+    public function cfgIncludeFont() { return $this->getConfigVar('font_include'); }
+    
+    ###########
+    ### CSS ###
+    ###########
+    public function onIncludeScripts()
+    {
+        $weight = $this->cfgFontWeight();
+        $this->addBowerCSS("fontsource-titillium-web/{$weight}.css"); # Font face
+        if ($this->cfgIncludeFont())
+        {
+            $this->addCSS('css/gdo6-titillium.css'); # Body style
+        }
+    }
+    
+}
---
+++ lang/titillium_en.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+'cfg_font_weight' => 'Font weight',
+'cfg_tt_font_weight' => 'Relative font weight.',    
+'cfg_font_include' => 'Include the font with a CSS rule?',
+);
